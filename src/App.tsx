767fe49c7484c3dacf2675e1d5d1f743827a7848
Sammy Teillet <sammy.teillet@gmail.com>
2024-07-10 16:02:12 +0200

    feature: add privacy page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Terms from './Terms';
+import Privacy from './Privacy';
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/terms" element={<Terms />} />
+                <Route path="/privacy" element={<Privacy />} />
             </Routes>
         </Router>
     );
